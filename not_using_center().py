b3a3874fc28bfd6ee3a2002aaeb144e30bb16f0c
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:58:14 +0800

    Add space to the left and right of the text.

--- not_using_center().py
+++ not_using_center().py
@@ -5,4 +5,10 @@ user_input = input("Enter a string: ")
 width = 50
 
 # Add spaces to both sides of the string to reach the fixed width
+spaces = width - len(user_input)
+if spaces > 0:
+    left_space = spaces // 2
+    right_space = spaces - left_space
+    user_input = " " * left_space + user_input + " " * right_space
+
 # Print the string with added spaces
