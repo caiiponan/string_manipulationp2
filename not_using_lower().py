bdaf6e5d5fcf4387efaee55e98309f8bf9b47ca7
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 19:50:48 +0800

    Initialize result with an empty string.

--- not_using_lower().py
+++ not_using_lower().py
@@ -2,6 +2,8 @@
 user_input = input("Enter a string with capital letters: ")
 
 # Initialize a variable to store the result
+result = ""
+
     # Check if character is uppercase letter
     # Convert to lowercase by adding 32 to ASCII value
 # Output with lowercase characters
