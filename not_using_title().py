b1968405bedd409fdd8f93a0bf4e2c4def3ca0cb
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 21:38:09 +0800

    Pseudo code for program.

---
+++ not_using_title().py
@@ -0,0 +1,7 @@
+# Prompt user for input
+# Initialize the result string as an empty string
+# Flag to decide when to capitalize
+# Check if every next character is uppercase
+# Make it uppercase
+ # Make it lowercase
+# Print title-cased result
