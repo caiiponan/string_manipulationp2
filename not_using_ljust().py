ed4d88b2be16f7e3ad244a257f06405e552c6928
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:42:24 +0800

    Add spaces with given width to the right of input.

--- not_using_ljust().py
+++ not_using_ljust().py
@@ -5,4 +5,9 @@ user_input = input("Enter a string: ")
 width = 50
 
 # Add spaces to the right of the string to reach the fixed width
+add_spaces = width - len(user_input)
+
+if add_spaces > 0:
+    user_input += " " * add_spaces
+
 # Print the string with added spaces
