0572d0bd74468579c1caa019b9bcba4d6a374451
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 21:09:54 +0800

    let user input string and initialize empty string.

--- not_using_swapcase().py
+++ not_using_swapcase().py
@@ -1,5 +1,9 @@
 # Prompt user for input
+user_input = input("Enter a string: ")
+
 # Initialize an empty string
+result = ""
+
 # Loop through the input string
 # If the character is lowercase
     # Convert to uppercase
