eeb0d379c2d0ed5d880553d9e3719a3eb3c9ae9d
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:21:09 +0800

    Pseudo code for program.

---
+++ not_using_endswith().py
@@ -0,0 +1,4 @@
+# Prompt user for input text
+# Let user specify if the text ends with the suffix
+# Get the length of the suffix
+# Compare the end part of the text with the suffix and print the result
