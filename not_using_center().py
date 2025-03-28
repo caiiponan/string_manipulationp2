cf77531ffe5e92a9390b71f87cfd623928a62a23
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:55:47 +0800

    Set fixed with to 50 and let user input a text.

---
+++ not_using_center().py
@@ -0,0 +1,8 @@
+# Prompt user for input
+user_input = input("Enter a string: ")
+
+# Set fixed width for the output
+width = 50
+
+# Add spaces to both sides of the string to reach the fixed width
+# Print the string with added spaces
