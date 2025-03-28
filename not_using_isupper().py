62c0e0532c67c283da7cd3ba8990d6f6a9848e19
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:17:16 +0800

    Set loop to find if a character is in lowercase.

--- not_using_isupper().py
+++ not_using_isupper().py
@@ -4,5 +4,10 @@ user_input = input("Enter a string: ")
 # Assume the user will enter a string with capital letters
 all_uppercase = True
 
+for char in user_input:
 # Find lowercase letters and set to false
+    if 'a' <= char <= 'z':
+        all_uppercase = False
+        break
+
 # Print result if all uppercase is true or false.
