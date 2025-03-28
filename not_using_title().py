27a75f9a3c13820eb708a67155e91c6a5e65dcb1
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 21:38:46 +0800

    Initialize result with an empty string.

--- not_using_title().py
+++ not_using_title().py
@@ -1,6 +1,7 @@
 # Prompt user for input
 user_input = input("Enter a string: ")
 # Initialize the result string as an empty string
+result = ""
 # Flag to decide when to capitalize
 # Check if every next character is uppercase
 # Make it uppercase
