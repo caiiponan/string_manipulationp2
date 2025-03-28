3f82af3dc771290874277c52ec121d1e358113d7
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 18:58:03 +0800

    Let user enter a string and prefix to be removed.

--- not_using_removeprefix().py
+++ not_using_removeprefix().py
@@ -1,3 +1,6 @@
 # Prompt for input text and the prefix to be removed
+user_input = input("Enter a string: ")
+prefix = input("Enter the prefix to be removed: ")
+
 # Remove given prefix without using removeprefix() method
 # Print the string without the prefix 
