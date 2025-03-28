957474e9fa7bcd21d3ed9691708f8f08cae71675
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:40:55 +0800

    Set fixed width to 50 characters.

--- not_using_ljust().py
+++ not_using_ljust().py
@@ -2,5 +2,7 @@
 user_input = input("Enter a string: ")
 
 # Set fixed width for the output
+width = 50
+
 # Add spaces to the right of the string to reach the fixed width
 # Print the string with added spaces
