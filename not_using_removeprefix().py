6010e9be810a87fb9526a8a86d3e4e6bf11ee90c
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 18:59:21 +0800

    Check to see if given prefix is present. Remove given prefix from the string if it is present.

--- not_using_removeprefix().py
+++ not_using_removeprefix().py
@@ -3,4 +3,7 @@ user_input = input("Enter a string: ")
 prefix = input("Enter the prefix to be removed: ")
 
 # Remove given prefix without using removeprefix() method
+if user_input.startswith(prefix):
+    user_input = user_input[len(prefix):]
+    
 # Print the string without the prefix 
