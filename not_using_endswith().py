b5a3e1c84a355a6cbe405a7ed9b6469538c8ba2a
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:21:43 +0800

    Let user enter a text and a suffix to check

--- not_using_endswith().py
+++ not_using_endswith().py
@@ -1,4 +1,8 @@
 # Prompt user for input text
+user_input = input("Enter a string: ")
+
 # Let user specify if the text ends with the suffix
+suffix = input("Enter the suffix to check: ")
+
 # Get the length of the suffix
 # Compare the end part of the text with the suffix and print the result
