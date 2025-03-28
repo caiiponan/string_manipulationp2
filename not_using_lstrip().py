aa050b9a69211777b0c67d7d591308cbbf30ae56
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 18:26:24 +0800

    Let user input a string with leading whitespaces.

--- not_using_lstrip().py
+++ not_using_lstrip().py
@@ -1,3 +1,5 @@
 # Prompt user input
+user_input = input("Enter a string with leading whitespaces: ")
+
 # Remove leading whitespaces without using lstrip() method
 # Print the string without leading whitespaces
