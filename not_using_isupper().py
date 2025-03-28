3d7caabd27b6b495aeee82dbf4d285676ebd4783
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:15:27 +0800

    Initialize that all the letters given is uppercase

--- not_using_isupper().py
+++ not_using_isupper().py
@@ -2,5 +2,7 @@
 user_input = input("Enter a string: ")
 
 # Assume the user will enter a string with capital letters
+all_uppercase = True
+
 # Find lowercase letters and set to false
 # Print result if all uppercase is true or false.
