0f28bab5eb6624547842692771ff4ee2b35dcaf3
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 20:40:33 +0800

    Let user enter input.

--- not_using_ljust().py
+++ not_using_ljust().py
@@ -1,4 +1,6 @@
 # Prompt user for input
+user_input = input("Enter a string: ")
+
 # Set fixed width for the output
 # Add spaces to the right of the string to reach the fixed width
 # Print the string with added spaces
