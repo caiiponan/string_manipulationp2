781ba7144e6b2e6e593f455b4a3a416cd5c237fa
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 21:12:20 +0800

    Reverse case the input given. Convert all lower case letters to upper case and vice versa by using the ACSII values of the characters and keep non-alphabetic characters as it is.

--- not_using_swapcase().py
+++ not_using_swapcase().py
@@ -5,9 +5,16 @@ user_input = input("Enter a string: ")
 result = ""
 
 # Loop through the input string
+for char in user_input:
 # If the character is lowercase
+    if 'a' <= char <= 'z':
     # Convert to uppercase
+        result += chr(ord(char) - 32)
 # Elif the character is uppercase
+    elif 'A' <= char <= 'Z':
     # Convert to lowercase
+        result += chr(ord(char) + 32)
 # Else keep non-alphabetic characters as they are
+    else:
+        result += char
 # Print the converted string
