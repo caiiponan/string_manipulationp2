bdd1a392a7267107b8758ec087bc9f8d2f8a558f
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 18:27:06 +0800

    Added in pseudo code to initialize count of space.

--- not_using_lstrip().py
+++ not_using_lstrip().py
@@ -1,5 +1,9 @@
 # Prompt user input
 user_input = input("Enter a string with leading whitespaces: ")
 
+# Initialize a variable to store the index of the first non-whitespace character
+index = 0
+
 # Remove leading whitespaces without using lstrip() method
+
 # Print the string without leading whitespaces
