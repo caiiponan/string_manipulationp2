1a7396d4a4914ff359d9b023d18f3be04b0f4512
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-03-28 21:31:32 +0800

    Let user enter a string.

--- not_using_capitalize().py
+++ not_using_capitalize().py
@@ -1,4 +1,6 @@
 # Prompt user for input
+user_input = input("Enter a string: ")
+
 # Initialize an empty string to store the result
 # Convert first character to uppercase 
 # Convert the rest to lowercase
